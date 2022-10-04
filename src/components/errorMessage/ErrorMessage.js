c09e8784ff9e23c58d7be521c578d24e62f43d9c
Inessa <inessa.boickova@yandex.ru>
2022-10-05 00:09:42 +0300

    create valid description, spinner and an error message

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,9 @@
+const ErrorMessage = () =>{
+    return (
+        <img alt = 'Error'
+        src = {process.env.PUBLIC_URL + '/error.gif'}
+        style = {{display:'block',width:'250px', height: '250px',objectFit: 'contain', margin: '0 auto'}}/>
+    )
+}
+
+export default ErrorMessage;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,46 +1,49 @@
 import './randomChar.scss';
+import Spinner from '../spinner/Spinner';
 import MarvelService from '../../services/MarvelService';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import mjolnir from '../../resources/img/mjolnir.png';
 import { useEffect, useState } from 'react';
 
 const RandomChar = () => {
     const [char, setChar] = useState({});
-    const {name,description,thumbnail,homepage,wiki} = char;
+    const [loading , setLoading] = useState(true);
+    const [error,setError] = useState(false);
 
     const marvelService = new MarvelService();
 
-    const onChatLoaded = (char) =>{
+    const onCharLoaded = (char) =>{
         setChar({...char});
+        setLoading(false)
     }
-
+    
     const upDateChar = ()=>{
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000 );
-        marvelService.getCharacter(id).then(onChatLoaded);
+        marvelService
+        .getCharacter(id)
+        .then(onCharLoaded)
+        .catch(OnError);
+    }
+
+    const OnError = () =>{
+        setLoading(false);
+        setError(true)
     }
 
     useEffect(() => {
         upDateChar();
     },[])
 
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error) ? <View char = {char}/> : null;
+
     return (
+    
         <div className="randomchar">
-            <div className="randomchar__block">
-                <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                <div className="randomchar__info">
-                    <p className="randomchar__name">{name}</p>
-                    <p className="randomchar__descr">
-                        {description}
-                    </p>
-                    <div className="randomchar__btns">
-                        <a href={homepage} className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href={wiki} className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
+            {errorMessage}
+            {spinner}
+            {content}
             <div className="randomchar__static">
                 <p className="randomchar__title">
                     Random character for today!<br/>
@@ -55,6 +58,29 @@ const RandomChar = () => {
                 <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
             </div>
         </div>
+    )
+}
+
+const View = (char) =>{
+    const {name,description,thumbnail,homepage,wiki} = char.char;
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {description}
+                </p>
+            <div className="randomchar__btns">
+                <a href={homepage} className="button button__main">
+                    <div className="inner">homepage</div>
+                </a>
+                <a href={wiki} className="button button__secondary">
+                    <div className="inner">Wiki</div>
+                </a>
+            </div>
+        </div>
+    </div>
     )
 }
 
