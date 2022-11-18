ec4b972f617b5b3bf7f01d69572f99f139576a5d
Inessa <inessa.boickova@yandex.ru>
2022-11-18 16:07:33 +0300

    create navigation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,38 +1,23 @@
-import { useState } from "react";
-import ComicsPage from "../comicsPage/comicsPage";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import decoration from '../../resources/img/vision.png';
+import MainPage from "../pages/MainPage";
+import CommicsPage from "../pages/ComicsPage";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 
 const App = () => {
-    const [selectedChar,setSelectedChar] = useState(null);
 
-    const onCharCelected = (id) =>{
-        setSelectedChar(id);
-    }
-    
     return (
-        <div className="app">
-            <AppHeader/> 
-            {/* <main>
-               <ErrorBoundary>
-                    <RandomChar/>
-              </ErrorBoundary>
-                <div className="char__content">
-                   <ErrorBoundary>
-                        <CharList onCharCelected= {onCharCelected}/>
-                   </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId ={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>  */}
-           <ComicsPage/>  
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/> 
+                <main>
+                    <Routes>
+                        <Route path='/' element={<MainPage/>}/>
+                        <Route path ='/comics' element={ <CommicsPage/>}/>
+                    </Routes>
+                </main> 
+            </div>
+        </Router>  
     )
 }
 
---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,13 @@
+import ComicsList from '../comicsList/ComicsList';
+import AppBanner from '../appBanner/AppBanner';
+
+const CommicsPage = () =>{
+    return (
+        <>
+            <AppBanner/>
+            <ComicsList/>
+        </>
+    )
+}
+
+export default CommicsPage;
